83a6f0e202087ef5794149cc699d1c249bf6e574
Adriano Finozzi Molero <adrianomolero@terra.com.br>
2020-05-07 23:57:12 -0300

    métodos de lista

--- index.php
+++ index.php
@@ -2,21 +2,24 @@
 
 require_once("config.php");
 
-$root = new Usuario();
-
 //die(print_r('aqui', true ));
 
+/* $root = new Usuario();
+  $root->loadById(3); 
+  echo $root;*/
 
-$root->loadById(3);
-
-echo $root;
-
-
+/*$lista = Usuario::getList();
+echo json_encode($lista);*/
 
 /*$sql = new Sql();
-
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios);*/
+
+/*$search = Usuario::search("s");
+echo json_encode($search);*/
+
+$usuario = new Usuario();
+$usuario->login("jose", "1234567890");
+echo $usuario;
 
 ?>
